73bdc109cce84a6331c266a20d21d9cfde8483e2
Angel R <93774378+Angelgaeta@users.noreply.github.com>
2023-08-28 14:00:13 +0200

    Récupération des data

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -14,12 +14,10 @@ use Symfony\Component\Serializer\SerializerInterface;
 class BookController extends AbstractController
 {
     #[Route('/api/books', name: 'book', methods: ['GET'])]
-    public function getBookList(BookRepository $bookRepository): JsonResponse
+    public function getBookList(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
     {
         $bookList = $bookRepository->findAll();
-
-        return new JsonResponse([
-            'books' => $bookList,
-        ]);
+        $jsonBookList = $serializer->serialize($bookList, 'json');
+        return new JsonResponse($jsonBookList, JsonResponse::HTTP_OK, [], true);
     }
 }
